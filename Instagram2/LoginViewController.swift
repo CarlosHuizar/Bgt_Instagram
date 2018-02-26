cf6d5a9af17c02b5fc0253d14aa8a342dbd43270
Carlos Huizar-Valenzuela <carlos_huizar@Carloss-MacBook-Pro.local>
2018-02-25 20:11:12 -0800

    add login and signup screen

---
+++ Instagram2/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  Instagram2
+//
+//  Created by Carlos Huizar-Valenzuela on 2/25/18.
+//  Copyright © 2018 Carlos Huizar-Valenzuela. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.signUpInBackground { (success: Bool,error: Error? ) -> Void in
+            if success{
+                print("Yaya, user created")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                
+            }else{
+                print(error?.localizedDescription)
+                if error?._code == 202{
+                    print("Username is taken")
+                }
+            }
+        }
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
